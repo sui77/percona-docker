b97e9e541de330abb0e40b5ac00e563d3c82941a
Suat Özgür <s.oezguer@gmail.com>
2022-10-19 15:20:39 +0200

    debugging

--- after-initdb.sh
+++ after-initdb.sh
@@ -4,14 +4,16 @@
 APPLICATION_PW=`< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c24`
 REPLICATION_PW=`< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c24`
 
-# Create user + database
-mysql --password="" -e "CREATE USER 'application'@'%' IDENTIFIED BY '$APPLICATION_PW';"
-mysql --password="" -e "CREATE DATABASE application";
-mysql --password="" -e "GRANT ALL PRIVILEGES ON application TO 'application'@'%'";
-mysql --password="" -e "GRANT REPLICATION SLAVE ON *.*  TO 'replication'@'%' IDENTIFIED BY '$REPLICATION_PW'";
-
 echo "APPLICATION_PW = ${APPLICATION_PW}"
 echo "REPLICATION_PW = ${REPLICATION_PW}"
+
+# Create user + database
+mysql -u root --password="" -e "CREATE USER 'application'@'%' IDENTIFIED BY '$APPLICATION_PW';"
+mysql -u root --password="" -e "CREATE DATABASE application";
+mysql -u root --password="" -e "GRANT ALL PRIVILEGES ON application TO 'application'@'%'";
+mysql -u root --password="" -e "GRANT REPLICATION SLAVE ON *.*  TO 'replication'@'%' IDENTIFIED BY '$REPLICATION_PW'";
+
+
 
 kubectl create secret generic $DBK8SNAME-credentials --namespace=$DBK8NAMESPACE --from-literal=APPLICATION_PW=$APPLICATION_PW --from-literal=REPLICATION_PW=$REPLICATION_PW
 echo "Done."
