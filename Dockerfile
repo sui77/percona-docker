b73795d7c86d4262062ad79a39126393ac57c36e
Suat Özgür <s.oezguer@gmail.com>
2022-10-19 16:30:36 +0200

    root pw

--- Dockerfile
+++ Dockerfile
@@ -8,7 +8,7 @@ RUN install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 
 FROM perconaRsync
 
-ENV MYSQL_RANDOM_ROOT_PASSWORD true
+ENV MYSQL_ROOT_PASSWORD=initpassword
 COPY after-initdb.sh /docker-entrypoint-initdb.d/after-initdb.sh
 RUN chmod 777 /docker-entrypoint-initdb.d/after-initdb.sh
 USER mysql
--- after-initdb.sh
+++ after-initdb.sh
@@ -3,19 +3,26 @@
 # Generate random password for application & replication
 APPLICATION_PW=`openssl rand -hex 20`
 REPLICATION_PW=`openssl rand -hex 20`
+ROOT_PW=`openssl rand -hex 20`
 
 echo "APPLICATION_PW = ${APPLICATION_PW}"
 echo "REPLICATION_PW = ${REPLICATION_PW}"
+echo "ROOT_PW = ${ROOT_PW}"
 
 # Create user + database
 echo "Creating user application"
-mysql -u root --password="" -e "CREATE USER 'application'@'%' IDENTIFIED BY '$APPLICATION_PW';"
-mysql -u root --password="" -e "CREATE DATABASE application;";
-mysql -u root --password="" -e "GRANT ALL PRIVILEGES ON application.* TO 'application'@'%'";
+mysql -u root --password="initpassword" -e "CREATE USER 'application'@'%' IDENTIFIED BY '$APPLICATION_PW';"
+mysql -u root --password="initpassword" -e "CREATE DATABASE application;";
+mysql -u root --password="initpassword" -e "GRANT ALL PRIVILEGES ON application.* TO 'application'@'%'";
 
 echo "Creating user replication"
-mysql -u root --password="" -e "CREATE USER 'replication'@'%' IDENTIFIED BY '$REPLICATION_PW';"
-mysql -u root --password="" -e "GRANT REPLICATION SLAVE ON *.* TO 'replication'@'%';"
+mysql -u root --password="initpassword" -e "CREATE USER 'replication'@'%' IDENTIFIED BY '$REPLICATION_PW';"
+mysql -u root --password="initpassword" -e "GRANT REPLICATION SLAVE ON *.* TO 'replication'@'%';"
 
+echo "Lock admin user from remote access"
+mysql -u root --password="initpassword" -e "ALTER USER 'root'@'%' IDENTIFIED BY '$ROOT_PW';"
+mysql -u root --password="initpassword" -e "ALTER USER 'root'@'%' IDENTIFIED BY '';"
+
+echo "Setting kubernetes secret"
 kubectl create secret generic $DBK8SNAME-credentials --namespace=$DBK8NAMESPACE --from-literal=APPLICATION_PW=$APPLICATION_PW --from-literal=REPLICATION_PW=$REPLICATION_PW
 echo "Done."
