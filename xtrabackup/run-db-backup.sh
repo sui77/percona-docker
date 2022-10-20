cdcc53de0a61e5dd07fae7124bab4bf4b08fff02
Suat Özgür <s.oezguer@gmail.com>
2022-10-20 22:10:52 +0200

    save output in backup.txt

--- xtrabackup/run-db-backup.sh
+++ xtrabackup/run-db-backup.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 FILENAME="backup-$(date +%Y-%m-%d-%H-%M)"
-xtrabackup --backup --datadir=/var/lib/mysql/ --target-dir=/backups/$FILENAME &> /backup.txt
+xtrabackup --backup --datadir=/var/lib/mysql/ --target-dir=/backups/$FILENAME &> /backups/backup.txt
 
 cd /backups
-tar -czvf $FILENAME.tar.gz -C /backups/$FILENAME . &> /backup.txt
+tar -czvf $FILENAME.tar.gz -C /backups/$FILENAME . &> /backups/backup.txt
 
 rm -rf /backups/$FILENAME
 
-rsync -avz /backups/ $RSYNC_TO &> /backup.txt
+rsync -avz /backups/ $RSYNC_TO &> /backups/backup.txt
 
 rm -rf /backups/$FILENAME.tar.gz
