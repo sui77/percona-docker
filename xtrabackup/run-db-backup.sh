cff075b15c18caaaf30737a85722a24b5ef41928
Suat Özgür <s.oezguer@gmail.com>
2022-10-20 20:58:24 +0200

    rsync fix

--- xtrabackup/run-db-backup.sh
+++ xtrabackup/run-db-backup.sh
@@ -3,10 +3,11 @@
 FILENAME="backup-$(date +%Y-%m-%d-%H-%M)"
 xtrabackup --backup --datadir=/var/lib/mysql/ --target-dir=/backups/$FILENAME
 
-cd /backups/$FILENAME
-tar -czvf ../$FILENAME.tar.gz /backups/$FILENAME
+cd /backups
+tar -czvf $FILENAME.tar.gz -C /backups/$FILENAME .
+
 rm -rf /backups/$FILENAME
 
-rsync -avz /backups $RSYNC_TO
+rsync -avz /backups/ $RSYNC_TO
 
 rm -rf /backups/$FILENAME.tar.gz
