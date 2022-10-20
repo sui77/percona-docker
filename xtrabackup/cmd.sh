7bc0de5eeb1fa4d7f34186a07d31fb53007a0a4f
Suat Özgür <s.oezguer@gmail.com>
2022-10-20 19:44:58 +0200

    wip

--- xtrabackup/cmd.sh
+++ xtrabackup/cmd.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+mkdir -r /backups
+
 # Install ssh keys if present
 if test -f "/data/sshkey/id_rsa"; then
     mkdir -p ~/.ssh
@@ -7,16 +9,20 @@ if test -f "/data/sshkey/id_rsa"; then
     chmod 600 ~/.ssh/*
 fi
 
-if [ -z ${CRON_SCHEDULE} ]; then
-    out_error "Missing env CRON_SCHEDULE"
+if [[ -z ${CRON_SCHEDULE} ]]; then
+    echo "Missing env CRON_SCHEDULE"
     exit 1
 fi
 
+if [[ -z ${RSYNC_TO} ]]; then
+    echo "Missing RSYNC_TO"
+    exit 1
+fi
 
 echo "Installing cron..."
 
 #crontab -e | { cat; echo "${CRON_SCHEDULE} mysql echo \"Hello\" "; } | crontab -
-(crontab -l 2>/dev/null; echo "${CRON_SCHEDULE} /run-db-backup.sh -with args") | crontab -
+(crontab -l 2>/dev/null; echo "${CRON_SCHEDULE} /run-db-backup.sh | /usr/bin/tee") | crontab -
 
 echo "Installing cron done."
 
--- xtrabackup/run-db-backup.sh
+++ xtrabackup/run-db-backup.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-date >> /tmp/date.txt
+FILENAME="backup-$(date +%Y-%m-%d-%H-%M)"
+xtrabackup --backup --datadir=/var/lib/mysql/ --target-dir=/backups/$FILENAME
+
+cd /backups/$FILENAME
+tar -czvf ../$FILENAME.tar.gz /backups/$FILENAME
+
+rsync -avz ./backups $RSYNC_TO
+
+rm -rf /backups/$FILENAME
+rm -rf /backups/$FILENAME.tar.gz
